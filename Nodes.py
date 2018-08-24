2b15a56e21b590fe2946c0cb043726766906c5e0
Vaughn Ramsey <vramsey20@slp-vramsey20.domain>
2018-08-23 19:46:45 -0600

    finished python driver

--- Nodes.py
+++ Nodes.py
@@ -1,46 +1,51 @@
 class Node:
-	nextNodes = null
 	def __init__(self, newData, pointer):
 		self.pointer = pointer
 		self.data = newData
-	def getData():
-		return data
+	def getData(self):
+		return self.data
 	def changeData(self, data):
 		self.data = data
 	def addNode(self, newNode):
 		self.pointer.append(newNode)
-	def rmNode(index):
-		del pointer[index]
-	def getXNode(index):
+	def rmNode(self, index):
+		del self.pointer[index]
+	def getXNode(self, index):
 		if(index<len(pointer)):
 			return pointer[index]
 trunk = Node("Main", [None])
 students = Node("Students", [None])
-teachers = Node("Teachers", [None])
 freshman = Node("Freshman", [None])
 sophomores = Node("Sophomores", [None])
 juniors = Node("Juniors", [None])
 seniors = Node("Seniors", [None])
-p1 = Node("Person 1", [None])
-p2 = Node("Person 2", [None])
-p3 = Node("Person 3", [None])
-p4 = Node("Person 4", [None])
-p5 = Node("Person 5", [None])
-p6 = Node("Person 6", [None])
-p7 = Node("Person 7", [None])
-p8 = Node("Person 8", [None])
-p9 = Node("Person 9", [None])
-p10 = Node("Person 10", [None])
-p11 = Node("Person 11", [None])
-p12 = Node("Person 12", [None])
-p13 = Node("Person 13", [None])
-p14 = Node("Person 14", [None])
-p15 = Node("Person 15", [None])
-p16 = Node("Person 16", [None])
+people = [Node("Person 1", [None]), Node("Person 2", [None]), Node("Person 3", [None]), Node("Person 4", [None]), Node("Person 5", [None]),
+Node("Person 6", [None]), Node("Person 7", [None]), Node("Person 8", [None]), Node("Person 9", [None]), Node("Person 10", [None]),
+Node("Person 11", [None]), Node("Person 12", [None]), Node("Person 13", [None]), Node("Person 14", [None]), Node("Person 15", [None]),
+Node("Person 16", [None])]
 trunk.addNode(students)
-trunk.addNode(teachers)
-
-
-print(test.pointer)
+students.addNode(freshman)
+students.addNode(sophomores)
+students.addNode(juniors)
+students.addNode(seniors)
+i = 0;
+for item in people:
+	if(i<4):
+		freshman.addNode(item)
+		i+=1
+	elif(i<8):
+		sophomores.addNode(item)
+		i+=1
+	elif(i<12):
+		juniors.addNode(item)
+		i+=1
+	elif(i<16):
+		seniors.addNode(item)
+		i+=1
+students.rmNode(0)
+for year in students.pointer:
+	year.rmNode(0)
+for student in juniors.pointer:
+	print(student.getData())
 
 
